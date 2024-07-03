c378b799d5e0152066136e2678177c43890600dd
Luiza Viana <l241126@dac.unicamp.br>
2024-07-03 10:39:58 -0300

    reading albums.json

--- src/main/java/com/echo/Application.java
+++ src/main/java/com/echo/Application.java
@@ -19,7 +19,7 @@ public class Application {
 
     public static void addAlbunsJson(ArrayList<Album> albums) {
        String json = "";
-       Artista taylorSwift = new Artista("Taylor Swift", "https://www.google.com/url?sa=i&url=https%3A%2F%2Finteligenciafinanceira.com.br%2Ffinancas%2Fquem-e%2Ftaylor-swift-the-eras-tour%2F&psig=AOvVaw1dOvf91xWqLQsHkNxmtNmi&ust=1720100331504000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNi1-Z3_iocDFQAAAAAdAAAAABAE")
+       Artista taylorSwift = new Artista("Taylor Swift", "https://www.google.com/url?sa=i&url=https%3A%2F%2Finteligenciafinanceira.com.br%2Ffinancas%2Fquem-e%2Ftaylor-swift-the-eras-tour%2F&psig=AOvVaw1dOvf91xWqLQsHkNxmtNmi&ust=1720100331504000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNi1-Z3_iocDFQAAAAAdAAAAABAE");
 
        try {
             json = new String(Files.readAllBytes(Paths.get("src/main/resources/albums.json")));
